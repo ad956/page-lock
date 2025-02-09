1adca4977f91cc283a428e8b5eb237c7879a0e88
Anand Suthar <anandsuthar956@gmail.com>
2025-02-09 17:51:15 +0530

    feat: set password ui

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,18 @@
 import { useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [password, setPassword] = useState("")
+
+  const savePassword = () => {
+    chrome.storage.local.set({ siteLockPassword: password })
+    alert("Password Saved")
+  }
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div>
+      <h2>Set Lock Password</h2>
+      <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={savePassword}>Save Password</button>
     </div>
   )
 }
