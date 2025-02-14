9da1991b9d2d4e40f75364361e32c87fd4379270
Anand Suthar <anandsuthar956@gmail.com>
2025-02-14 17:42:19 +0530

    reafctor: move all files to src

--- utils/normalizeUrl.ts
+++
@@ -1,5 +0,0 @@
-export const normalizeUrl = (url: string): string => {
-  let normalized = url.replace(/^(https?:\/\/)?(www\.)?/, "")
-  normalized = normalized.replace(/\/.*$/, "")
-  return normalized.toLowerCase()
-}
